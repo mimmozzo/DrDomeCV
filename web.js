37ea83fa40e2d4329aa229f0a709fb9adfd303c7
Mimmo <domenico.riccio@gmail.com>
2013-10-09 01:05:39 +0200

    Initial CV version

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
-
-app.get('/', function(request, response) {
-    response.send('Hello DrDome!');
+app.configure(function(){
+  app.use('/media', express.static(__dirname + '/media'));
+  app.use(express.static(__dirname + '/public'));
 });
+
+/*
+app.get('/', function(request, response) {
+  response.send('Hello DrDome!');
+});
+*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
